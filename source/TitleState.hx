e423a54144f531fdb81b32eb0814c2ecc8738077
ImTariq <tariq2007nl@gmail.com>
2021-04-22 12:34:52 +0200

    oops forgot to load in the saves

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -45,6 +45,7 @@ class TitleState extends MusicBeatState
 	override public function create():Void
 	{
 		Polymod.init({modRoot: "mods", dirs: ['introMod']});
+		SaveData.saveLoad();
 
 		#if (!web)
 		TitleState.soundExt = '.ogg';
